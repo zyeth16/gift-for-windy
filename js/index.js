70056862af3f521807b996b24486183bb718cce0
Dyaz <daffaalfahrezy5@gmail.com>
2026-07-31 12:30:50 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const title = document.querySelector('.title')
+const text = ``.split('')
+
+// Create container for better responsive layout
+title.style.display = 'flex'
+title.style.flexWrap = 'wrap'
+title.style.justifyContent = 'center'
+title.style.gap = '0.5rem'
+
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}</span>`
+  } else {
+    title.innerHTML += `<span style='width: 1rem'></span>`
+  }
+}
+
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3;
+  element.style.animationDelay = `${randomDelay}s`;
+});
